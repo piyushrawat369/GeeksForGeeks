84d2fd510bd50c2189b67605983c8d8c6d7061b5
piyushrawat369 <78429520+piyushrawat369@users.noreply.github.com>
2022-12-29 01:46:55 +0530

    Create code.cpp

---
+++ Daily Challenge/Convert array into Zig-Zag fashion/code.cpp
@@ -0,0 +1,65 @@
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+  public:
+    // Program for zig-zag conversion of array
+    void zigZag(int arr[], int n) {
+        bool flag = true;
+ 
+        for(int i=0; i<=n-2; i++){
+            if(flag){
+                if(arr[i] > arr[i+1])
+                    swap(arr[i], arr[i+1]);
+                
+            }
+            else{
+                if(arr[i] < arr[i+1])
+                    swap(arr[i], arr[i+1]);
+                    
+            }
+            flag = !flag;
+            
+        }
+    }
+};
+
+bool isZigzag(int arr[], int n){
+    int f=1;
+    
+    for(int i=1; i<n; i++){
+        if(f){
+            if(arr[i-1]>arr[i]) return 0;
+        }
+        else{
+            if(arr[i-1]<arr[i]) return 0;
+        }
+        f=f^1;
+    }
+    
+    return 1;
+}
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        ob.zigZag(arr, n);
+        bool check=1;
+        check=isZigzag(arr,n);
+        
+        if(check) cout<<"1\n";
+        else cout<<"0\n";
+        
+    }
+    return 0;
+}
