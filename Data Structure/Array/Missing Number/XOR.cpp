e428df83b6dc6aaa331fb52b20282c36c5df3346
piyushrawat369 <78429520+piyushrawat369@users.noreply.github.com>
2023-04-30 21:16:59 +0530

    Create XOR.cpp

---
+++ Data Structure/Array/Missing Number/XOR.cpp
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int missingNumber(int a[], int n);
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int i=0, n;
+		cin>>n;
+		int a[n+5];
+		for(i=0;i<n-1;i++)
+			cin>>a[i];
+			
+		cout<<missingNumber(a, n)<<endl;
+	}
+}
+// } Driver Code Ends
+
+
+int missingNumber(int a[], int n)
+{
+    int xor1 = 0, xor2 = 0;
+    for(int i=0; i<n-1; i++){
+        xor1 = xor1^a[i];
+        xor2 = xor2^(i+1);
+    }
+    xor2 = xor2^n;
+    
+    return xor1^xor2;
+}
